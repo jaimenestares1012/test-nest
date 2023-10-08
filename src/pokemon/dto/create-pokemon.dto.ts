623db37d111e5b6f274f4990abf2dd10faf9f841
JAimeenestarees321 <jaimenestares1012@gmail.com>
2023-10-08 03:00:13 -0500

    fistr

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, Min, IsString, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  // @IsNumber
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+
+  @IsString()
+  @MinLength(2)
+  name: string;
+}
---
+++ src/seed/seed.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+  async executeSeed() {
+    const { data } = await axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    );
+    console.log('data', data);
+    data.results.forEach(({ name, url }) => {
+      const sengments = url.split('/');
+      console.log('sengments', sengments);
+      const no = +sengments[sengments.length - 2]
+    });
+    return data.results;
+  }
+}
